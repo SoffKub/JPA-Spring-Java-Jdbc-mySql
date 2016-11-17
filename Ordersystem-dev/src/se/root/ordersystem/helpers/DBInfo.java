0b1c00f155d7eafb0c7d261b7180a47f595314b8
sofia <prog.15.sk_outlook.com>
2016-11-17 13:21:59 +0100

    OrderSystem
    WorkItem,User,Team,Issue

---
+++ Ordersystem-dev/src/se/root/ordersystem/helpers/DBInfo.java
@@ -0,0 +1,9 @@
+package se.root.ordersystem.helpers;
+
+public class DBInfo {
+
+	public static final String url = "jdbc:mysql://127.0.0.1:3307/ordersystem?useSSL=false";
+	public static final String databaseUsername = "root";
+	public static final String databasePassword = "";
+
+}
